cdba3685b949643d4c7a5ac926e0c7477710ffc7
Ivan <144352886+int422@users.noreply.github.com>
2025-12-04 20:59:02 +0700

    Refactor veb.py for code clarity and structure

--- veb.py
+++ veb.py
@@ -189,4 +189,5 @@ def profile_photo(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
